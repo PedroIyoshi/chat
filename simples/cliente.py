023ea79577a0822bf21f13f4155ff642bad5dd0b
piyoshi <piyoshi@empro.com.br>
2021-05-19 00:44:59 -0300

    Implementada a interface gráfica ao cliente

--- simples/cliente.py
+++ simples/cliente.py
@@ -1,55 +1,58 @@
-from socket import socket,AF_INET,SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
+from tkinter import *
+from sys import exit
 from gui import interface
 
-#classe para manipular o socket
+
+def mensagem(text):
+    send.put('Você: ' + text)
+
+
+def desconecta():
+    root.destroy()
+    tcp.close()
+
+
+root = Tk()
+root.protocol("WM_DELETE_WINDOW", desconecta)
+gui = interface(root, mensagem)
+
+
 class Send:
- def __init__(self):
-  self.__msg=''
-  self.new=True
-  self.con=None
- def put(self,msg):
-  self.__msg=msg
-  if self.con != None:
-   #envia um mensagem atravez de uma conexão socket
-   self.con.send(str.encode(self.__msg))
- def get(self):
-  return self.__msg
- def loop(self):
-  return self.new
- 
-#função esperar - Thread
-def esperar(tcp,send,host='localhost',port=5000):
- destino=(host,port)
- #conecta a um servidor
- tcp.connect(destino)
-  
- while send.loop():
-  print('Conectado a ',host,'.')
-  #atribui a conexão ao manipulador
-  send.con=tcp
-  while send.loop():
-   #aceita uma mensagem
-   msg=tcp.recv(1024)
-   if not msg: break
-   print(str(msg,'utf-8'))
- 
+    def __init__(self):
+        self.__msg = ''
+        self.new = True
+        self.con = None
+
+    def put(self, texto):
+        self.__msg = texto
+        if self.con is not None:
+            self.con.send(str.encode(self.__msg))
+
+    def get(self):
+        return self.__msg
+
+    def loop(self):
+        return self.new
+
+
+def esperar(tcp, send, host='localhost', port=5000):
+    tcp.connect((host, port))
+    while send.loop():
+        gui.printMessage('Conectado a ' + host + '.')
+        send.con = tcp
+        while send.loop():
+            msg = tcp.recv(1024)
+            if not msg:
+                break
+            gui.printMessage('Outro: ' + str(msg, 'utf-8'))
+
+
 if __name__ == '__main__':
- host = '127.0.0.1'
-  
- #cria um socket
- tcp=socket(AF_INET,SOCK_STREAM)
- send=Send()
- #cria um Thread e usa a função esperar com dois argumentos
- processo=Thread(target=esperar,args=(tcp,send,host))
- processo.start()
- print('')
-  
- msg=input()
- while True:
-  send.put(msg)
-  msg=input()
-  
- processo.join()
- tcp.close()
- exit()
+    host = '127.0.0.1'
+    tcp = socket(AF_INET, SOCK_STREAM)
+    send = Send()
+    processo = Thread(target=esperar, args=(tcp, send, host))
+    processo.start()
+    root.mainloop()
--- simples/gui.py
+++ simples/gui.py
@@ -1,19 +1,20 @@
 from tkinter import *
 from tkinter import scrolledtext
-from random import randint
+import socket
 
 
-class interface():
-    def __init__(self, master=None):
+class interface:
+    def __init__(self, master=None, send=None):
         master.config(width=620, height=380)
-        self.componentes()
+        self.componentes(send)
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def componentes(self):
+    def componentes(self, send):
         self.chat = scrolledtext.ScrolledText()
         self.mensagem = scrolledtext.ScrolledText()
         self.mensagem.vbar.pack_forget()
-        self.enviar = Button(text="Enviar", command= lambda: 
-            self.sendText(str(self.mensagem.get(0.0, END))))
+        self.enviar = Button(text="Enviar", command=lambda:
+            self.sendText(send, str(self.mensagem.get(0.0, END))))
 
         self.chat.config(state=DISABLED)
         self.chat.place(x=10, y=10, width=600, height=300)
@@ -22,13 +23,10 @@ class interface():
 
     def printMessage(self, texto):
         self.chat.config(state=NORMAL)
-        self.chat.insert(INSERT, "Você: " + texto)
+        self.chat.insert(INSERT, texto)
         self.chat.config(state=DISABLED)
 
-    def sendText(self, texto):
+    def sendText(self, send, texto):
+        send(texto)
         self.printMessage(texto)
 
-root = Tk()
-root.title("Chat")
-interface(root)
-root.mainloop()
